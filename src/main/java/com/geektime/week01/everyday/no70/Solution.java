e1240cd48cf89cd1a3a8615300f024a1470f5ac1
bruce <mxz0828@163.com>
2020-11-30 11:38:23 +0800

    'no70_leetcode'

---
+++ src/main/java/com/geektime/week01/everyday/no70/Solution.java
@@ -0,0 +1,9 @@
+package com.geektime.week01.everyday.no70;/**
+*   ClassName:Solution
+*   PackageName:com.geektime.week01.everyday.no70
+*   Description:
+*   @date:2020/11/30 11:28
+*   @authorBy:mxzmxzmxz0828@gmail.com
+*/
+public class Solution {
+}
