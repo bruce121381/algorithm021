3c3b8741a02a40ecdb17ce2f535113f2b600859e
bruce <mxz0828@163.com>
2020-11-30 12:11:17 +0800

    'no11_leetcode'

---
+++ src/main/java/com/geektime/week01/practical/no11/Solution.java
@@ -0,0 +1,22 @@
+package com.geektime.week01.practical.no11;
+
+/**
+ * ClassName:Solution
+ * PackageName:com.geektime.week01.practical.no11
+ * Description:
+ *
+ * @date:2020/11/30 12:08
+ * @authorBy:mxzmxzmxz0828@gmail.com
+ */
+public class Solution {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            int minHeight = height[i] > height[j] ? height[j--] : height[i++];
+            max = Math.max(max, (j - i) * minHeight);
+        }
+        return max;
+    }
+
+}
